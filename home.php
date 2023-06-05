fdd358ec663599a4eececb897a14dc976266c80f
watermin <tnals08019@gmail.com>
2023-06-06 07:26:58 +0900

    [feat] 헤더 구성

---
+++ home.php
@@ -0,0 +1,21 @@
+<!DOCTYPE html>
+<html>
+    <head>
+        <meta charset="utf-8">
+        <title>VeganMall 🌱</title>
+        <link rel="stylesheet" type="text/css" href="./styles/style.css">
+        <link rel="stylesheet" type="text/css" href="./styles/home.css">
+    </head>
+    <body>
+        <header>
+            <!-- include : 파일을 현재 위치에 삽입하는 역할 -->
+            <?php include "header.php"; ?>
+        </header>
+        <section>
+            <?php include "home_content.php"; ?>
+        </section>
+        <footer>
+            <?php include "footer.php"; ?>
+        </footer>
+    </body>
+</html>
