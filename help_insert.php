07153a16d97a5f90941b63844d4d06f9de450298
watermin <tnals08019@gmail.com>
2023-06-08 06:11:17 +0900

    [fix] 한국 timezone으로 설정

--- help_insert.php
+++ help_insert.php
@@ -1,5 +1,7 @@
 <meta charset="utf-8">
 <?php
+    date_default_timezone_set('Asia/Seoul');
+
     session_start();
     if (isset($_SESSION["user_id"])) $user_id = $_SESSION["user_id"];
     else $user_id = "";
