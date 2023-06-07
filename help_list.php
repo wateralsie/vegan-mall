bd529e0f84bb1b258edebf995a843b6469fe9cb3
watermin <tnals08019@gmail.com>
2023-06-08 08:21:21 +0900

    [feat] 1:1문의 목록 구현

---
+++ help_list.php
@@ -0,0 +1,131 @@
+<!DOCTYPE html>
+<html>
+
+<head>
+    <meta charset="utf-8">
+    <title>VeganMall 🌱</title>
+    <link rel="stylesheet" type="text/css" href="./styles/style.css">
+    <link rel="stylesheet" type="text/css" href="./styles/help.css">
+</head>
+
+<body>
+    <header>
+        <?php include "header.php"; ?>
+    </header>
+    <section>
+        <div id="main_img_bar">
+            <img src="./image/main_img.png">
+        </div>
+        <div id="board_box">
+            <h3>
+                고객센터 > 1:1문의 목록
+            </h3>
+            <ul id="board_list">
+                <li>
+                    <span class="col1">번호</span>
+                    <span class="col2">제목</span>
+                    <span class="col3">글쓴이</span>
+                    <span class="col4">첨부</span>
+                    <span class="col5">등록일</span>
+                </li>
+                <?php
+                if (isset($_GET["page"]))
+                    $page = $_GET["page"];
+                else
+                    $page = 1;
+
+                $connect = mysqli_connect("localhost", "user1", "12345", "vegan_mall");
+                $sql = "select * from helps order by help_id desc";
+                $result = mysqli_query($connect, $sql);
+                $total_record = mysqli_num_rows($result); // 전체 글 수
+
+                $scale = 10;
+
+                // 전체 페이지 수($total_page) 계산 
+                if ($total_record % $scale == 0)
+                    $total_page = floor($total_record / $scale);
+                else
+                    $total_page = floor($total_record / $scale) + 1;
+
+                // 표시할 페이지($page)에 따라 $start 계산  
+                $start = ($page - 1) * $scale;
+
+                $number = $total_record - $start;
+
+                for ($i = $start; $i < $start + $scale && $i < $total_record; $i++) {
+                    mysqli_data_seek($result, $i);
+                    // 가져올 레코드로 위치(포인터) 이동
+                    $row = mysqli_fetch_array($result);
+                    // 하나의 레코드 가져오기
+                    $help_id     = $row["help_id"];
+                    $id          = $row["username"];
+                    $name        = $row["name"];
+                    $subject     = $row["subject"];
+                    $created_at  = $row["created_at"];
+                    if ($row["file_name"])
+                        $file_image = "<img src='./image/file.gif'>";
+                    else
+                        $file_image = " ";
+                ?>
+                    <li>
+                        <span class="col1"><?= $number ?></span>
+                        <span class="col2"><a href="help_view.php?help_id=<?= $help_id ?>&page=<?= $page ?>"><?= $subject ?></a></span>
+                        <span class="col3"><?= $name ?></span>
+                        <span class="col4"><?= $file_image ?></span>
+                        <span class="col5"><?= $created_at ?></span>
+                    </li>
+                <?php
+                    $number--;
+                }
+                mysqli_close($conect);
+
+                ?>
+            </ul>
+            <ul id="page_num">
+                <?php
+                if ($total_page >= 2 && $page >= 2) {
+                    $new_page = $page - 1;
+                    echo "<li><a href='help_list.php?page=$new_page'>◀ 이전</a> </li>";
+                } else
+                    echo "<li>&nbsp;</li>";
+
+                // 게시판 목록 하단에 페이지 링크 번호 출력
+                for ($i = 1; $i <= $total_page; $i++) {
+                    if ($page == $i)     // 현재 페이지 번호 링크 안함
+                    {
+                        echo "<li><b> $i </b></li>";
+                    } else {
+                        echo "<li><a href='help_list.php?page=$i'> $i </a><li>";
+                    }
+                }
+                if ($total_page >= 2 && $page != $total_page) {
+                    $new_page = $page + 1;
+                    echo "<li> <a href='help_list.php?page=$new_page'>다음 ▶</a> </li>";
+                } else
+                    echo "<li>&nbsp;</li>";
+                ?>
+            </ul>
+            <ul class="buttons">
+                <li><button onclick="location.href='help_list.php'">목록</button></li>
+                <li>
+                    <?php
+                    if ($userid) {
+                    ?>
+                        <button onclick="location.href='help_form.php'">글쓰기</button>
+                    <?php
+                    } else {
+                    ?>
+                        <a href="javascript:alert('로그인 후 이용해 주세요!')"><button>글쓰기</button></a>
+                    <?php
+                    }
+                    ?>
+                </li>
+            </ul>
+        </div>
+    </section>
+    <footer>
+        <?php include "footer.php"; ?>
+    </footer>
+</body>
+
+</html>
