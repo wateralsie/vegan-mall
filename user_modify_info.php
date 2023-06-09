db7a56b753219150d4239869a2fb5bd581acab3e
watermin <tnals08019@gmail.com>
2023-06-09 19:36:26 +0900

    [ui] 회원가입 디자인 적용

--- user_modify_info.php
+++ user_modify_info.php
@@ -5,7 +5,7 @@
         <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>VeganMall 🌱</title>
         <link rel="stylesheet" type="text/css" href="./styles/style.css">
-        <link rel="stylesheet" type="text/css" href="./styles/member.css">
+        <link rel="stylesheet" type="text/css" href="./styles/signup.css">
         <script type="text/javascript" src="./js/user_modify.js"></script>
         <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
         <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
@@ -31,9 +31,6 @@
         mysqli_close($connect);
     ?>
         <section>
-            <div id="main_img_bar">
-                <img src="./image/main_img.png">
-            </div>
             <div id="main_content">
                 <div id="join_box">
                 <form name="user_form" method="post" action="user_modify.php?id=<?=$user_id?>">
@@ -69,7 +66,6 @@
                                     type="text" name="email_domain" value="<?=$email_domain?>">
                             </div>                 
                         </div>
-                        <div class="bottom_line"> </div>
                         <div class="buttons">
                             <img style="cursor:pointer" src="./image/button_save.gif" onclick="check_input()">&nbsp;
                             <img id="reset_button" style="cursor:pointer" src="./image/button_reset.gif"
