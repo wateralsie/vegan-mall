a9f323ee97cf3c0f441ae32c6dc0dc5b667df352
watermin <tnals08019@gmail.com>
2023-06-09 15:52:29 +0900

    [ui] 로그인 화면 디자인

--- login.php
+++ login.php
@@ -15,27 +15,7 @@
             <?php include "header.php"; ?>
         </header>
         <section>
-            <div id="main_img_bar">
-                <img src="./image/main_img.png">
-            </div>
-            <div id="main_content">
-                <div id="login_box">
-                    <div id="login_title">
-                        <span>로그인</span>
-                    </div>
-                    <div id="login_form">
-                        <form name="login" action="login_execute.php" method="post">
-                            <ul>
-                                <li><input type="text" name="id" placeholder="아이디"></li>
-                                <li><input type="password" name="pw" placeholder="비밀번호"></li>
-                            </ul>
-                            <div id="login_btn">
-                                <a href="#"><img src="./image/login.png" onclick="check_input()"></a>
-                            </div>
-                        </form>
-                    </div>
-                </div>
-            </div>
+            <?php include "login_content.php"; ?>
         </section>
         <footer>
             <?php include "footer.php"; ?>
