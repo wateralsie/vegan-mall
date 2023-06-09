95e6000306e768946003495d5edb1c8a1cb64a74
watermin <tnals08019@gmail.com>
2023-06-09 14:52:21 +0900

    [add] 다른 파일도 bootstrap 필수 코드 넣기

--- login.php
+++ login.php
@@ -2,10 +2,13 @@
 <html>
     <head>
         <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
         <title>VeganMall 🌱</title>
         <link rel="stylesheet" type="text/css" href="./styles/style.css">
         <link rel="stylesheet" type="text/css" href="./styles/login.css">
         <script type="text/javascript" src="./js/login.js"></script>
+        <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-9ndCyUaIbzAi2FUVXJi0CjmCapSmO7SnpJef0486qhLnuZ2cdeRhO02iuK6FUUVM" crossorigin="anonymous">
+        <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js" integrity="sha384-geWF76RCwLtnZ8qwWowPQNguL3RmwHVBC9FhGdlKrxdiJJigb/j/68SIy3Te4Bkz" crossorigin="anonymous"></script>
     </head>
     <body>
         <header>
